32cf3a0748c25d330cc4b9811518827fb193c44a
Felix Ifland <felix.ifland@mni.thm.de>
2023-01-26 15:41:20 +0100

    Updates Argument parding

--- cmd/wikidata-importer/main.go
+++ cmd/wikidata-importer/main.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"flag"
-	"log"
-
 	"git.thm.de/gsas42/wikidata-importer/pkg/importer"
+	"log"
 )
 
 func main() {
 	neo4jAddress := flag.String("neo4j", "neo4j://localhost:7687", "neo4j connection string")
 	neo4jUser := flag.String("user", "neo4j", "neo4j user")
 	neo4jPassword := flag.String("password", "1234", "neo4j password")
-	stage := flag.Int("stage", -1, "which stage to run")
+	stage := flag.Int("stage", -1, "start at stage / which stage to run")
 	dumpPath := flag.String("dump", "dump", "path of the dump to load or save")
+	singleStage := flag.Bool("single_stage", false, "Only run the selected stage, do not continue")
 
 	flag.Parse()
 
-	if *stage == -1 {
-		log.Panic("Please select which stage to run!")
+	if *stage < 0 || *stage > 3 {
+		log.Panic("Please select a valid stage to run!")
 	}
 
 	importer, err := importer.NewWikidataImporter(&importer.Neo4JConfig{
@@ -31,39 +31,47 @@ func main() {
 
 	defer importer.Close()
 
-	if *stage == 0 {
+	if *stage <= 0 {
 		err = importer.RunStage0()
 		if err != nil {
 			log.Panic(err)
 		}
+
+		if *singleStage {
+			return
+		}
 	}
 
-	if *stage == 1 {
+	if *stage <= 1 {
 		err = importer.RunStage1()
 		if err != nil {
 			log.Panic(err)
 		}
+
+		if *singleStage {
+			return
+		}
 	}
 
-	if *stage == 2 {
+	if *stage <= 2 {
 		err = importer.RunStage2()
 		if err != nil {
 			log.Panic(err)
 		}
+
+		if *singleStage {
+			return
+		}
 	}
 
-	if *stage == 3 {
+	if *stage <= 3 {
 		err = importer.RunStage3()
 		if err != nil {
 			log.Panic(err)
 		}
-	}
 
-	// mediawiki.ProcessWikidataDump(ctx, &config, func(c context.Context, entity mediawiki.Entity) errors.E {
-	//   fmt.Printf("ID: %v\n", entity.ID)
-	//   // if entity.ID == "Q103" {
-	//   //   fmt.Printf("Claims: %v\n", entity.Claims["P136"][0].References[0])
-	//   // }
-	//   return nil
-	// })
+		if *singleStage {
+			return
+		}
+	}
 }
